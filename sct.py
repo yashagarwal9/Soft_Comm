f7f086b50811dace7bdb6bfa4f219472f083ad0f
vidit98 <vidit.goel9816@gmail.com>
2019-04-11 22:08:27 +0530

    Save model

--- sct.py
+++ sct.py
@@ -7,152 +7,108 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 # Read data from file 'filename.csv' 
 # (in the same directory that your python process is based)
-# Control delimiters, rows, column names with read_csv (see later) 
-data = pd.read_csv("sct_data.csv") 
+# Control delimiters, rows, column names with read_csv (see later)  3.375 azad 2.333 hjb 1.88 nehru
+def create_data(file_h):
+    data = pd.read_csv(file_h) 
 
-day = data["What Day is it today?"]
+    day = data["What Day is it today?"]
 
-fresh = data["What was the condition of the vegetables before cooking?"]
+    fresh = data["What was the condition of the vegetables before cooking?"]
 
-animals = data["Is the mess free from animals like cats and dogs?"] 
+    animals = data["Is the mess free from animals like cats and dogs?"] 
 
-expirence = data["How experienced is the mess worker who is cooking the food?"]
+    expirence = data["How experienced is the mess worker who is cooking the food?"]
 
-peeled = data["Were the vegetables washed and peeled properly?"]
+    peeled = data["Were the vegetables washed and peeled properly?"]
 
-cleanliness = data["How would you rate the overall cleanliness of the mess?"]
+    cleanliness = data["How would you rate the overall cleanliness of the mess?"]
 
-roti_wastage = data["Roti / Paratha wastage (food left over in the plates)"]
+    roti_wastage = data["Roti / Paratha wastage (food left over in the plates)"]
 
-dal_wastage = data["Dal wastage (food left over in the plates)"]
+    dal_wastage = data["Dal wastage (food left over in the plates)"]
 
-sabji_wastage = data["Sabji wastage (food left over in the plates)"]
+    sabji_wastage = data["Sabji wastage (food left over in the plates)"]
 
-rice_wastage = data["Rice Wastage (food left over in the plates)"]
+    rice_wastage = data["Rice Wastage (food left over in the plates)"]
+    comments = data["Why do you think there will be food wastage in your mess today?"]
 
-#print (rice_wastage)
-day_crisp = np.zeros((day.size, 7), dtype=int)
+    #print (rice_wastage)
+    day_crisp = np.zeros((day.size, 7), dtype=int)
 
-fresh_fuzzy = np.zeros((fresh.size, 3),  dtype=float)
+    fresh_fuzzy = np.zeros((fresh.size, 3),  dtype=float)
 
-animals_crisp = np.zeros(animals.size, dtype=int)
-
-
-peeled_fuzzy = np.zeros((peeled.size, 3) ,dtype=float)
-experience_fuzzy = np.zeros((peeled.size, 4) ,dtype=float)
-
-cleanliness_fuzzy = np.zeros((cleanliness.size, 3), dtype=float)
-
-day_arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-day_bin = [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1,0,0,0], [0,0,0,0,1,0,0], [0,0,0,0,0,1,0], [0,0,0,0,0,0,1]]
-
-day_crisp = [day_bin[day_arr.index(i)] for i in day]
-
-fresh_mem = [[-1,-1,1], [-1,1,1]]
-
-fresh_fuzzy = [fresh_mem[1] if i == "Fresh" else fresh_mem[0] for i in fresh ]
-
-animals_crisp = [[-1] if i == "Yes" else [1] for i in animals]
-
-peeled_fuzzy = [fresh_mem[0] if i == "Yes" else fresh_mem[1] for i in peeled]
-
-cleanliness_mem = [[-1,-1,0], [-0.5,0,0.5], [0,1,1]]
-
-clean_arr = ["Dirty", "Average", "Excellent"]
-
-fresh_fuzzy = [fresh_mem[0] if i == "Fresh" else fresh_mem[1] for i in fresh ]
-
-cleanliness_fuzzy =  [cleanliness_mem[clean_arr.index(i)] for i in cleanliness]
-
-experience_mem = [[-1,-1,-0.83333, -0.5], [-0.83333, -0.5, -.1666, .1666], [-.1666, .1666, 0.5, 0.83333], [0.5, .8333, 1,1]]
-
-exp_arr = ["<1 Year", "1-3 Years", "3-5 Years", ">5 Years"]
-
-experience_fuzzy =  [experience_mem[exp_arr.index(i)] for i in expirence]
-print(type(day_crisp))
-final_data = [fresh_fuzzy[i]+animals_crisp[i]+peeled_fuzzy[i]+cleanliness_fuzzy[i]+experience_fuzzy[i] for i in range(56)]
-
-rng_state = np.random.get_state()
-final_data = np.array(final_data)
-np.random.shuffle(final_data)
-
-#wastage_mem = [[-1,-1,0.5], [-1,-0.5,0], [-0.5,0 0.5], [0,0.5, 1], [0.5, 1,1]]
-wastage = [[1], [2], [3], [4], [5]]
-
-wastage_arr = [1,2,3,4,5]
-
-roti_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in roti_wastage])
-
-sabji_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in sabji_wastage])
-
-dal_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in dal_wastage])
-
-rice_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in rice_wastage]) 
-print(roti_wastage_output)
-wastage_output = np.add(0.4*roti_wastage_output, 0.6*sabji_wastage_output)#, np.add(0.1*dal_wastage_output, 0.1*rice_wastage_output)) #[roti_wastage_output[i] + sabji_wastage_output[i] + dal_wastage_output[i] + rice_wastage_output[i]  for i in range(7)]
-np.random.set_state(rng_state)
-wastage_output = np.array(wastage_output)
-np.random.shuffle(wastage_output)
-
-data = pd.read_csv("sct_data2.csv") 
-
-day = data["What Day is it today?"]
-
-fresh = data["What was the condition of the vegetables before cooking?"]
-
-animals = data["Is the mess free from animals like cats and dogs?"] 
-
-expirence = data["How experienced is the mess worker who is cooking the food?"]
-
-peeled = data["Were the vegetables washed and peeled properly?"]
-
-cleanliness = data["How would you rate the overall cleanliness of the mess?"]
-
-roti_wastage = data["Roti / Paratha wastage (food left over in the plates)"]
-
-dal_wastage = data["Dal wastage (food left over in the plates)"]
-
-sabji_wastage = data["Sabji wastage (food left over in the plates)"]
-
-rice_wastage = data["Rice Wastage (food left over in the plates)"]
+    animals_crisp = np.zeros(animals.size, dtype=int)
 
 
-day_crisp = [day_bin[day_arr.index(i)] for i in day]
+    peeled_fuzzy = np.zeros((peeled.size, 3) ,dtype=float)
+    experience_fuzzy = np.zeros((peeled.size, 4) ,dtype=float)
 
-fresh_fuzzy = [fresh_mem[1] if i == "Fresh" else fresh_mem[0] for i in fresh ]
+    cleanliness_fuzzy = np.zeros((cleanliness.size, 3), dtype=float)
 
-animals_crisp = [[-1] if i == "Yes" else [1] for i in animals]
+    # day_arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    # day_bin = [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1,0,0,0], [0,0,0,0,1,0,0], [0,0,0,0,0,1,0], [0,0,0,0,0,0,1]]
 
-peeled_fuzzy = [fresh_mem[0] if i == "Yes" else fresh_mem[1] for i in peeled]
+    # day_crisp = [day_bin[day_arr.index(i)] for i in day]
 
-fresh_fuzzy = [fresh_mem[0] if i == "Fresh" else fresh_mem[1] for i in fresh ]
+    fresh_mem = [[-1,-1,1], [-1,1,1]]
 
-cleanliness_fuzzy =  [cleanliness_mem[clean_arr.index(i)] for i in cleanliness]
+    fresh_fuzzy = [fresh_mem[1] if i == "Fresh" else fresh_mem[0] for i in fresh ]
 
-experience_fuzzy =  [experience_mem[exp_arr.index(i)] for i in expirence]
+    animals_crisp = [[-1] if i == "Yes" else [1] for i in animals]
 
-final_data_2 = [fresh_fuzzy[i]+animals_crisp[i]+peeled_fuzzy[i]+cleanliness_fuzzy[i]+experience_fuzzy[i] for i in range(8)]
+    comments_crisp = [[1] if "Bad Taste" in i else [-1] for i in comments]
 
-#rng_state = np.random.get_state()
+    peeled_fuzzy = [fresh_mem[0] if i == "Yes" else fresh_mem[1] for i in peeled]
+
+    cleanliness_mem = [[-1,-1,0], [-0.5,0,0.5], [0,1,1]]
+
+    clean_arr = ["Dirty", "Average", "Excellent"]
+
+    fresh_fuzzy = [fresh_mem[0] if i == "Fresh" else fresh_mem[1] for i in fresh ]
+
+    cleanliness_fuzzy =  [cleanliness_mem[clean_arr.index(i)] for i in cleanliness]
+
+    experience_mem = [[-1,-1,-0.83333, -0.5], [-0.83333, -0.5, -.1666, .1666], [-.1666, .1666, 0.5, 0.83333], [0.5, .8333, 1,1]]
+
+    exp_arr = ["<1 Year", "1-3 Years", "3-5 Years", ">5 Years"]
+
+    experience_fuzzy =  [experience_mem[exp_arr.index(i)] for i in expirence]
+    print(type(day_crisp))
+    final_data = [comments_crisp[i] + fresh_fuzzy[i]+animals_crisp[i]+peeled_fuzzy[i]+cleanliness_fuzzy[i]+experience_fuzzy[i] for i in range(len(animals_crisp))]
+
+    rng_state = np.random.get_state()
+    final_data = np.array(final_data)
+    # np.random.shuffle(final_data)
+
+    #wastage_mem = [[-1,-1,0.5], [-1,-0.5,0], [-0.5,0 0.5], [0,0.5, 1], [0.5, 1,1]]
+    wastage = [[1], [2], [3], [4], [5]]
+
+    wastage_arr = [1,2,3,4,5]
+
+    roti_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in roti_wastage])
+
+    sabji_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in sabji_wastage])
+
+    dal_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in dal_wastage])
+
+    rice_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in rice_wastage]) 
+    print(roti_wastage_output)
+    wastage_output = np.add(0.4*roti_wastage_output, 0.6*sabji_wastage_output)#, np.add(0.1*dal_wastage_output, 0.1*rice_wastage_output)) #[roti_wastage_output[i] + sabji_wastage_output[i] + dal_wastage_output[i] + rice_wastage_output[i]  for i in range(7)]
+    
+    return final_data, wastage_output
+
+final_data, wastage_output = create_data("sct_data.csv")
+final_data_2, wastage_output_2 = create_data("sct_data2.csv")
+
+
 final_data_2 = np.array(final_data_2)
-#np.random.shuffle(final_data)
 
-print(final_data.shape)
 
-roti_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in roti_wastage])
 
-sabji_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in sabji_wastage])
 
-dal_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in dal_wastage])
-
-rice_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in rice_wastage])
-
-wastage_output_2 = np.add(0.4*roti_wastage_output, 0.6*sabji_wastage_output)# np.add(0.1*dal_wastage_output, 0.1*rice_wastage_output)) #[roti_wastage_output[i] + sabji_wastage_output[i] + dal_wastage_output[i] + rice_wastage_output[i]  for i in range(7)]
-#np.random.set_state(rng_state)
 wastage_output_2 = np.array(wastage_output_2)
-#np.random.shuffle(wastage_output)
 
-input_size = 14
+input_size = 15
 hidden_size = [40, 8]
 num_classes = 1
 learning_rate = 0.001
@@ -174,37 +130,37 @@ class NeuralNet(nn.Module):
         return out
     
 nu = 50
-model = NeuralNet(input_size, hidden_size, num_classes)#.to(device)
+model = torch.load("Model")#NeuralNet(input_size, hidden_size, num_classes)#.to(device)
+model.eval()
 final_data = final_data.astype('float32')
-# final_data_train = torch.from_numpy(final_data[0:50,:])
-# final_data_test = torch.from_numpy(final_data[50:56,:])
+
 final_data_2 = final_data_2.astype('float32')
 final_data_train = torch.from_numpy(final_data)
 final_data_test = torch.from_numpy(final_data_2)
 
 wastage_output = wastage_output.astype('float32')
 wastage_output_2 = wastage_output_2.astype('float32')
-# wastage_output_train = torch.from_numpy(wastage_output[0:50,:])
-# wastage_output_test = torch.from_numpy(wastage_output[50:56,:])
+
 wastage_output_train = torch.from_numpy(wastage_output)
 wastage_output_test = torch.from_numpy(wastage_output_2)
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
 
 total_step = 56
-for epoch in range(num_epochs):
-    	#for i, (final_data, wastage_output) in enumerate(final_data):
-        #final_data = final_data.to(device)
-        #wastage_output = wastage_output.to(device)
-        # print(final_data.shape)
-        outputs = model(final_data_train)
-        loss = criterion(outputs, wastage_output_train)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        #if (i+1) % 10 == 0:
-        print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
 
+# for epoch in range(num_epochs):
+#     	#for i, (final_data, wastage_output) in enumerate(final_data):
+#         #final_data = final_data.to(device)
+#         #wastage_output = wastage_output.to(device)
+#         # print(final_data.shape)
+#         outputs = model(final_data_train)
+#         loss = criterion(outputs, wastage_output_train)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
+#         #if (i+1) % 10 == 0:
+#         print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+# torch.save(model, "Model")
 with torch.no_grad():
     correct = 0 
     total = 0
