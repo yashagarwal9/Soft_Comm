e00678027e373cbbb81c89b4acde58c525fa15b1
arowdy98 <arathore98@gmail.com>
2019-04-12 20:37:31 +0530

    Trained model

--- sct.py
+++ sct.py
@@ -18,7 +18,7 @@ def create_data(file_h):
     animals = data["Is the mess free from animals like cats and dogs?"] 
 
     expirence = data["How experienced is the mess worker who is cooking the food?"]
-
+    quality = data["Quality of ingredients used in making food"]
     peeled = data["Were the vegetables washed and peeled properly?"]
 
     cleanliness = data["How would you rate the overall cleanliness of the mess?"]
@@ -45,10 +45,10 @@ def create_data(file_h):
 
     cleanliness_fuzzy = np.zeros((cleanliness.size, 3), dtype=float)
 
-    # day_arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    # day_bin = [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1,0,0,0], [0,0,0,0,1,0,0], [0,0,0,0,0,1,0], [0,0,0,0,0,0,1]]
+    day_arr = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    day_bin = [[1,0,0,0,0,0,0], [0,1,0,0,0,0,0], [0,0,1,0,0,0,0], [0,0,0,1,0,0,0], [0,0,0,0,1,0,0], [0,0,0,0,0,1,0], [0,0,0,0,0,0,1]]
 
-    # day_crisp = [day_bin[day_arr.index(i)] for i in day]
+    day_crisp = [day_bin[day_arr.index(i)] for i in day]
 
     fresh_mem = [[-1,-1,1], [-1,1,1]]
 
@@ -73,18 +73,24 @@ def create_data(file_h):
     exp_arr = ["<1 Year", "1-3 Years", "3-5 Years", ">5 Years"]
 
     experience_fuzzy =  [experience_mem[exp_arr.index(i)] for i in expirence]
-    print(type(day_crisp))
-    final_data = [comments_crisp[i] + fresh_fuzzy[i]+animals_crisp[i]+peeled_fuzzy[i]+cleanliness_fuzzy[i]+experience_fuzzy[i] for i in range(len(animals_crisp))]
+
+    qual_mem = [[-1,-1,-0.5], [-1,-0.5,0], [-0.5,0 ,0.5], [0,0.5, 1], [0.5, 1,1]]
+
+    wastage_arr = [1,2,3,4,5]
+
+    qual_fuzzy = [qual_mem[wastage_arr.index(i)] for i in quality]
+
+    final_data = [comments_crisp[i]+fresh_fuzzy[i]+animals_crisp[i]+cleanliness_fuzzy[i]+qual_fuzzy[i] + experience_fuzzy[i] for i in range(len(animals_crisp))]
+	# final_data = [day_crisp[i] + comments_crisp[i]+fresh_fuzzy[i]+animals_crisp[i]+peeled_fuzzy[i]+cleanliness_fuzzy[i]+experience_fuzzy[i] for i in range(len(animals_crisp))]
 
     rng_state = np.random.get_state()
     final_data = np.array(final_data)
     # np.random.shuffle(final_data)
 
-    #wastage_mem = [[-1,-1,0.5], [-1,-0.5,0], [-0.5,0 0.5], [0,0.5, 1], [0.5, 1,1]]
+    
     wastage = [[1], [2], [3], [4], [5]]
 
-    wastage_arr = [1,2,3,4,5]
-
+    
     roti_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in roti_wastage])
 
     sabji_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in sabji_wastage])
@@ -92,7 +98,7 @@ def create_data(file_h):
     dal_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in dal_wastage])
 
     rice_wastage_output = np.array([wastage[wastage_arr.index(i)] for i in rice_wastage]) 
-    print(roti_wastage_output)
+
     wastage_output = np.add(0.4*roti_wastage_output, 0.6*sabji_wastage_output)#, np.add(0.1*dal_wastage_output, 0.1*rice_wastage_output)) #[roti_wastage_output[i] + sabji_wastage_output[i] + dal_wastage_output[i] + rice_wastage_output[i]  for i in range(7)]
     
     return final_data, wastage_output
@@ -100,41 +106,35 @@ def create_data(file_h):
 final_data, wastage_output = create_data("sct_data.csv")
 final_data_2, wastage_output_2 = create_data("sct_data2.csv")
 
-
-final_data_2 = np.array(final_data_2)
-
-
-
-
-wastage_output_2 = np.array(wastage_output_2)
-
-input_size = 15
-hidden_size = [40, 8]
-num_classes = 1
+input_size = final_data.shape[1]
+hidden_size = [80,8]
+num_classes = wastage_output.shape[1]
 learning_rate = 0.001
-num_epochs = 4000
+num_epochs = 5000
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size[0])
-        # self.fc2 = nn.Linear(hidden_size[0], hidden_size[1])
-        self.fc3 = nn.Linear(hidden_size[0], num_classes)
+        self.fc2 = nn.Linear(hidden_size[0], hidden_size[1])
+        self.fc3 = nn.Linear(hidden_size[1], num_classes)
         self.relu = nn.ReLU()
     def forward(self, x):
         out = self.fc1(x)
-        # out = self.relu(out)
-        # out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc2(out)
         out = self.relu(out)
         out = self.fc3(out)
         return out
     
-nu = 50
-model = torch.load("Model")#NeuralNet(input_size, hidden_size, num_classes)#.to(device)
+model = torch.load("Model_CQ")
 model.eval()
-final_data = final_data.astype('float32')
 
+# model = NeuralNet(input_size, hidden_size, num_classes)#.to(device)
+
+final_data = final_data.astype('float32')
 final_data_2 = final_data_2.astype('float32')
+
 final_data_train = torch.from_numpy(final_data)
 final_data_test = torch.from_numpy(final_data_2)
 
@@ -143,6 +143,7 @@ wastage_output_2 = wastage_output_2.astype('float32')
 
 wastage_output_train = torch.from_numpy(wastage_output)
 wastage_output_test = torch.from_numpy(wastage_output_2)
+
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(),lr = learning_rate)
 
@@ -160,7 +161,12 @@ total_step = 56
 #         optimizer.step()
 #         #if (i+1) % 10 == 0:
 #         print ('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
-# torch.save(model, "Model")
+
+# torch.save(model, "Model_CQ5")
+
+#Model_C - with comments and no days
+#Model2 - with comments as well as days
+
 with torch.no_grad():
     correct = 0 
     total = 0
@@ -212,6 +218,7 @@ with torch.no_grad():
     t1 = np.linspace(0, 2*outputs.shape[0], outputs.shape[0])
     a1 = outputs
     b1 = np.array(wastage_output_test)
+    print(np.sum(np.square(b1-outputs)))
 
     # a1 = outputs[:,1]
     # b1 = np.array(wastage_output_test)[:,1]
@@ -243,7 +250,5 @@ plt.plot(t1, b1, c="red") # plotting t, c separately
 # plt.scatter(t, a3) # plotting t, a separately 
 # plt.scatter(t, b3,s=2,c="red") # plotting t, b separately 
 # plt.plot(t, b3, c="red") # plotting t, c separately 
-plt.show()
-
-        
+plt.show()      
    
